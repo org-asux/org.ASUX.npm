28fd54578b8bc14b7dbd2e7636f363c4cfa8654b
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2020-03-20 23:53:46 -0400

    FIX: finally tested UN-install on a brand new Macbook

--- bin/asux-uninstall.js
+++ bin/asux-uninstall.js
@@ -5,6 +5,6 @@
 
 var fs = require("fs"); 			// https://nodejs.org/api/fs.html#fs_fs_accesssync_path_mode
 var os = require('os');				// https://nodejs.org/api/os.html
-fs.rmdirSync( os.homedir() +".asux.org", {"maxRetries": 1, "recursive": true, "retryDelay": 5000 } );
+fs.rmdirSync( os.homedir() +"/.asux.org", {"maxRetries": 1, "recursive": true, "retryDelay": 5000 } );
 
 //EOF
