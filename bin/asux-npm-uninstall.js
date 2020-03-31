ae7810821fde442330f1d81961174aabcbbd9e11
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2020-03-31 19:30:28 -0400

    ORGASUXFLDR changed from ~/.asux.org to ~/.org.asux

--- bin/asux-npm-uninstall.js
+++ bin/asux-npm-uninstall.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 // echo Correct way to invoke $0 is via the command: npm uninstall asux
-// rm -rf ~/.asux.org
+// rm -rf ~/.org.asux
 
 var fs = require("fs"); 			// https://nodejs.org/api/fs.html#fs_fs_accesssync_path_mode
 var os = require('os');				// https://nodejs.org/api/os.html
-fs.rmdirSync( os.homedir() +"/.asux.org", {"maxRetries": 1, "recursive": true, "retryDelay": 5000 } );
+fs.rmdirSync( os.homedir() +"/.org.asux", {"maxRetries": 1, "recursive": true, "retryDelay": 5000 } );
 
 //EOF
