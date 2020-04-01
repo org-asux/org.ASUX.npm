cb1197e1680933e6774289910c81e96a5fb690b4
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2020-03-31 21:27:05 -0400

    FEAT: fs.rmdirSync() with {"recursive": true} does NOT work.  added a simple DIY function to remove ~/.org.asux folder

--- bin/asux-npm-uninstall.js
+++ bin/asux-npm-uninstall.js
@@ -5,6 +5,25 @@
 
 var fs = require("fs"); 			// https://nodejs.org/api/fs.html#fs_fs_accesssync_path_mode
 var os = require('os');				// https://nodejs.org/api/os.html
-fs.rmdirSync( os.homedir() +"/.org.asux", {"maxRetries": 1, "recursive": true, "retryDelay": 5000 } );
+
+// fs.rmdirSync( os.homedir() +"/.org.asux", {"maxRetries": 1, "recursive": true, "retryDelay": 5000 } );
+    // Error: ENOTEMPTY: directory not empty, rmdir '/Users/admin/.org.asux'
+    //     at Object.fs.rmdirSync (fs.js:821:3)
+
+var deleteFolderRecursive = function(path) {
+    if( fs.existsSync(path) ) {
+        fs.readdirSync(path).forEach(function(file,index){
+            var curPath = path + "/" + file;
+            if(fs.lstatSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
+};
+
+deleteFolderRecursive( os.homedir() +"/.org.asux" );
 
 //EOF
